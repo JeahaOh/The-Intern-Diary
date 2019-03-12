3fdf111c0b67ca6cb348407d75b149d1f5d538c0
JeahaOh <jeaha1217@gmail.com>
2019-03-12 15:56:32 +0900

    .

--- com.yummy/src/main/java/egovframework/util/Rater.java
+++ com.yummy/src/main/java/egovframework/util/Rater.java
@@ -5,15 +5,17 @@ import org.springframework.stereotype.Component;
 @Component("rater")
 public class Rater {
   
-  //  총 평가 수
-  int tot;
-  //  별로예요
-  int bad;
-  //  그저 그래요
-  int soso;
-  //  좋아요
+  int cnt;
+  int sum;
+  
+  float grade;
+  
+  //  강추, 맛집, 무난, 별로, 최악
+  int best;
   int good;
-  //  
-  float avg;
+  int soso;
+  int bad;
+  int worst;
+  
   
 }
