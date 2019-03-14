3d8a5b75b57304a05a063d3fabdddda6e902bf31
JeahaOh <jeaha1217@gmail.com>
2019-03-14 17:22:39 +0900

    03.14 17:20

--- ManagementAndroidStudies/app/src/main/java/com/management/RegisterActivity.java
+++ ManagementAndroidStudies/app/src/main/java/com/management/RegisterActivity.java
@@ -1,9 +1,19 @@
 package com.management;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,11 +22,55 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        EditText idText = (EditText) findViewById(R.id.idText);
-        EditText passwordText = (EditText) findViewById(R.id.passwordText);
-        EditText nameText = (EditText) findViewById(R.id.nameText);
-        EditText ageText = (EditText) findViewById(R.id.ageText);
+        final EditText idText = (EditText) findViewById(R.id.idText);
+        final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        final EditText nameText = (EditText) findViewById(R.id.nameText);
+        final EditText ageText = (EditText) findViewById(R.id.ageText);
 
         Button registerButton = (Button) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String userId = idText.getText().toString();
+                String userPassword = passwordText.getText().toString();
+                String userName = nameText.getText().toString();
+                int userAge = Integer.parseInt(ageText.getText().toString());
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if( success ) {
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("회원 가입 성공")
+                                        .setPositiveButton("확인", null)
+                                        .create()
+                                        .show();
+
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }   else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("회원 가입 실패")
+                                        .setPositiveButton("다시시도", null)
+                                        .create()
+                                        .show();
+                            }
+                        }   catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+                RegisterRequest registerRequest = new RegisterRequest(userId, userPassword, userName, userAge,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
