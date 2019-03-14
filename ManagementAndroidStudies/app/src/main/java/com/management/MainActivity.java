d4c31342f15aa63006a96a6b3ed54c72a584d18d
JeahaOh <jeaha1217@gmail.com>
2019-03-14 16:31:59 +0900

    03.14 16:30

--- ManagementAndroidStudies/app/src/main/java/com/management/MainActivity.java
+++ ManagementAndroidStudies/app/src/main/java/com/management/MainActivity.java
@@ -2,6 +2,8 @@ package com.management;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+
+        TextView welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
     }
 }
